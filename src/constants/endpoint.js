fd48a49b390c86694d13c5d76c90da6f213bcc27
Phuong Dao Hoai <daophuong131.work@gmail.com>
2025-02-06 17:15:12 +0700

    update: endpoint v2

--- src/constants/endpoint.js
+++ src/constants/endpoint.js
@@ -3,29 +3,6 @@ const endpoints = {
 	REGISTER: "/register",
 	SEND_OTP: "/send-otp",
 	VERIFY_OTP_CHANGE_PASSWORD: "/verify-otp-and-change-password",
-<<<<<<< HEAD
-	GET_INFOR_USER: "/get-infor",
-	CREATE_APPOINTMENT: "/create-appointment",
-	APPROVE_APPOINTMENT: "/approve/:appointmentId",
-	REJECT_APPOINTMENT: "/reject/:appointmentId",
-	CREATE_FEEDBACK: "/create-feedback",
-	ADD_FAVORITE_STYLIST: "/add-favorite-stylist",
-	RECOMMENDED_STYLIST: "/recommended-stylists",
-	CREATE_SERVICE: "/create-service",
-	VIEW_SERVICE: "/view-service",
-	UPDATE_SERVICE: "/update-service",
-	DELETE_SERVICE: "/delete-service",
-	UPDATE_USER: "/update-user",
-	GET_USER_ROLE: "/get-user-role",
-	CREATE_VOUCHER: "/create-voucher",
-	VIEW_VOUCHER: "/view-voucher",
-	UPDATE_VOUCHER: "/update-voucher",
-	DELETE_VOUCHER: "/delete-voucher",
-	VIEW_STYLISTS: "/get-all-stylists",
-	SCHEDULE_STYLISTS_APPOINTMENTS: "/available-time-slots",
-	GET_APPOINTMENT_USER: "/get-appointment-user",
-};
-=======
 	GET_PROFILE_USER: "/get-profile",
 	UPDATE_PROFILE_USER: "/update-profile",
 	CHANGE_PASSWORD: "/change-password",
@@ -49,5 +26,4 @@ const endpoints = {
 	DELETE_QUIZ: "/delete-quiz",
 };
 
->>>>>>> origin/master
 export default endpoints;
