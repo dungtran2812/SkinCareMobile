38e7c8a5feac7203172b11a60c881bc53efe4959
Phuong Dao Hoai <daophuong131.work@gmail.com>
2025-02-28 02:51:10 +0700

    update: style img brand logo

--- src/components/brand/BrandImages.jsx
+++ src/components/brand/BrandImages.jsx
@@ -1,4 +1,12 @@
-// BrandImages.js
 export const brandImages = {
 	brand1: require("../../../assets/images/brand/CeraVe.png"),
+	brand2: require("../../../assets/images/brand/Laroche-Posay.png"),
+	brand3: require("../../../assets/images/brand/bioderma.png"),
+	brand4: require("../../../assets/images/brand/uriage.png"),
+	brand5: require("../../../assets/images/brand/loreal.png"),
+	brand6: require("../../../assets/images/brand/vichy.png"),
+	brand7: require("../../../assets/images/brand/bio.png"),
+	brand8: require("../../../assets/images/brand/cocoon.png"),
+	brand9: require("../../../assets/images/brand/eucerin.png"),
+	brand10: require("../../../assets/images/brand/klairs.png"),
 };
