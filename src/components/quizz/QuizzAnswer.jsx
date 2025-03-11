a67391ae2a061908cc3e4e96df1b9152dbeb8ace
Phuong Dao Hoai <daophuong131.work@gmail.com>
2025-03-12 01:27:38 +0700

    update: QuizzAnswer

--- src/components/quizz/QuizzAnswer.jsx
+++ src/components/quizz/QuizzAnswer.jsx
@@ -1,12 +1,36 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import CombinationSkin from "./../skin/CombinationSkin";
+import DrySkin from "./../skin/DrySkin";
+import NormalSkin from "./../skin/NormalSkin";
+import OilySkin from "./../skin/OilySkin";
 
 const QuizzAnswer = ({ route }) => {
 	const { skinType } = route.params;
 
+	let SkinComponent;
+	switch (skinType) {
+		case "dry":
+			SkinComponent = DrySkin;
+			break;
+		case "combination":
+			SkinComponent = CombinationSkin;
+			break;
+		case "normal":
+			SkinComponent = NormalSkin;
+			break;
+		case "oily":
+			SkinComponent = OilySkin;
+			break;
+		default:
+			SkinComponent = () => (
+				<Text style={styles.text}>Không xác định được loại da</Text>
+			);
+	}
+
 	return (
 		<View style={styles.container}>
-			<Text style={styles.text}>Loại da của bạn là: {skinType}</Text>
+			<SkinComponent />
 		</View>
 	);
 };
