7186a881ead516dcf7321c958075e3f7409cc792
Phuong Dao Hoai <daophuong131.work@gmail.com>
2025-03-14 19:35:29 +0700

    style: Order Success and navigation

--- src/components/order/OrderSuccess.jsx
+++ src/components/order/OrderSuccess.jsx
@@ -1,31 +1,29 @@
 import React from "react";
 import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import Ionicons from "react-native-vector-icons/Ionicons"; // Import Ionicons
 
 const OrderSuccess = () => {
 	const navigation = useNavigation();
 
 	return (
 		<View style={styles.container}>
+			<Ionicons
+				name="checkmark-circle-outline"
+				size={100}
+				color="#1E90FF"
+			/>
 			<Text style={styles.title}>Bạn đã đặt đơn thành công!</Text>
-			<View style={styles.buttonContainer}>
-				<TouchableOpacity
-					style={styles.button}
-					onPress={() => navigation.navigate("Home")}
-				>
-					<Text style={styles.buttonText}>Quay về trang chủ</Text>
-				</TouchableOpacity>
-				<TouchableOpacity
-					style={styles.button}
-					onPress={() =>
-						navigation.navigate("OrderStatus", { position: 0 })
-					}
-				>
-					<Text style={styles.buttonText}>
-						Ghé xem đơn hàng của tôi
-					</Text>
-				</TouchableOpacity>
-			</View>
+			<TouchableOpacity
+				style={styles.button}
+				onPress={() =>
+					navigation.navigate("MainTabNavigator", {
+						screen: "HomeScreen",
+					})
+				}
+			>
+				<Text style={styles.buttonText}>Quay về trang chủ</Text>
+			</TouchableOpacity>
 		</View>
 	);
 };
@@ -42,21 +40,15 @@ const styles = StyleSheet.create({
 		fontSize: 24,
 		fontWeight: "bold",
 		color: "#000",
-		marginBottom: 20,
+		marginVertical: 20,
 		textAlign: "center",
-	},
-	buttonContainer: {
-		flexDirection: "row",
-		justifyContent: "space-around",
-		width: "100%",
 	},
 	button: {
 		backgroundColor: "#1E90FF",
 		padding: 15,
 		borderRadius: 5,
 		alignItems: "center",
-		flex: 1,
-		marginHorizontal: 10,
+		width: "80%",
 	},
 	buttonText: {
 		color: "#fff",
