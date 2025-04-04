0dae10cd1725d4a0ea437e88f7914deb0bd4a31f
dungtran2812 <dunghoclaptrinh2812@gmail.com>
2025-04-04 19:45:32 +0700

    Auto stash before checking out "origin/master"

--- src/store/store.js
+++ src/store/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import { persistStore } from "redux-persist";
 import rootReducer from "../redux/rootReducer";
 import skincareApi from "../services/skincare.service";
+import devtoolsEnhancer from "redux-devtools-expo-dev-plugin";
 
 const store = configureStore({
 	reducer: {
 		rootReducer,
 		[skincareApi.reducerPath]: skincareApi.reducer,
 	},
+	enhancers: (getDefaultEnhancers) => getDefaultEnhancers().concat(devtoolsEnhancer()),
 	middleware: getDefaultMiddleware =>
 		getDefaultMiddleware({
 			serializableCheck: false,
